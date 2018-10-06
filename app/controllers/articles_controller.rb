08d5d224e140441ba5498fdd21da612d946a343f
vinu <vinu@ex.com>
2018-10-06 16:44:45 +0000

    almost done

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -9,7 +9,12 @@ class ArticlesController < ApplicationController
     def show
         @article=Article.find(params[:id])
     end
-    
+    def destroy
+     @article=Article.find(params[:id])
+     @article.destroy
+     flash[:notice]="Article is destroyed"
+     redirect_to articles_path
+    end
     def edit
         @article=Article.find(params[:id])
     end
